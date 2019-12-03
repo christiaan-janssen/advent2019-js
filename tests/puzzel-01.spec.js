3f1e50a972b47f46f4dd83f786c83bf580cd9c55
Christiaan Janssen <christiaanjanssen@outlook.com>
2019-12-03 11:25:25 +0100

    Tests defined for puzzle 01 - B

--- tests/puzzel-01.spec.js
+++ tests/puzzel-01.spec.js
@@ -1,4 +1,4 @@
-import { calculateMass } from '../puzzels/puzzel-01.js';
+import { calculateMass, calculateFuelForFuel } from '../puzzels/puzzel-01.js';
 
 describe('Calculate a modules mass', () => {
   test('mass of 12', () => {
@@ -14,3 +14,15 @@ describe('Calculate a modules mass', () => {
     expect(calculateMass(100756)).toEqual(33583);
   });
 });
+
+describe('Calculate fuel for fuel', () => {
+  test('Fuel for fuel for a mass of 14', () => {
+    expect(calculateFuelForFuel(14)).toEqual(0);
+  })
+  test('Fuel for fuel for a mass of 1969', () => {
+    expect(calculateFuelForFuel(1969)).toEqual(966);
+  })
+  test('Fuel for fuel for a mass of 100756', () => {
+    expect(calculateFuelForFuel(100756)).toEqual(50346);
+  })
+})
